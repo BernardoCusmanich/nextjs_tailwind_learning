c9c63b43b635104f1721643012ec7a622d27410e
sirshadow <bernardoxd2@gmail.com>
2022-10-18 19:57:48 -0300

    Arrumando o código

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <div className='bg-white fixed top-0 w-[100%] z-10'>
+            <div className='container mx-auto flex justify-between items-center py-4'>
+                <div className='text-[24px]'>Bernardo Cusmanich</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
