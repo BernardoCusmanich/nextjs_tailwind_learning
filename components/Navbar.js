a71304cda9bcd6310e4276101f790bcf43400147
sirshadow <bernardoxd2@gmail.com>
2022-10-18 21:15:51 -0300

    Consertos e bugs

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,24 @@
+import Link from "next/link";
 import React from "react"
 
 const Navbar = () => {
     return (
-        <div className='bg-white fixed top-0 w-[100%] z-10'>
+        <div className='bg-blue-500 fixed top-0 w-[100%] z-10'>
             <div className='container mx-auto flex justify-between items-center py-4'>
                 <div className='text-[24px]'>Bernardo Cusmanich</div>
+                <div className='flex gap-6'>
+                    
+                        <a className='hover:text-[red]'>Home</a>
+                    
+                        <a className='hover:text-[red]'>About</a>
+                        
+                        <a className='hover:text-[red]'>Portfolio</a>
+
+                        <a className='hover:text-[red]'>Blog</a>
+                    
+                        <a className='hover:text-[red]'>Contact</a>
+
+                </div>
             </div>
         </div>
     )
